09e50b6e9a8bec8c4f8968ecb65ec9d9e1ada090
Mark Brown <marksbrown@gmail.com>
2014-10-01 11:25:51 -0400

    Update menu buttons and finish navigation

--- Assets/Scripts/MenuButtonManager.cs
+++ Assets/Scripts/MenuButtonManager.cs
@@ -6,11 +6,42 @@ public class MenuButtonManager : MonoBehaviour {
 
 	void OnMouseDown()
 	{
+		switch(this.name)
+		{
+			case "MainMenu_Credits":
+				HandleCreditClick();
+				break;
+			case "MainMenu_Exit":
+				Application.Quit();
+				break;
+			case "Credits_Back":
+				HandleBackClick();
+				break;
+			default:
+				HandleGameDifficultyClick();
+				break;
+		}
+	}
+
+	private void HandleGameDifficultyClick()
+	{
 		var mainMenuManager = transform.parent.GetComponent<MainMenuManager>();
 		mainMenuManager.SetGameDifficulty(GameDifficulty);
-
-	    audio.PlayOneShot(MainMenuSelect);
-
+		
+		audio.PlayOneShot(MainMenuSelect);
+		
 		Application.LoadLevel("Game");
+	}
+
+	private void HandleCreditClick()
+	{
+		audio.PlayOneShot(MainMenuSelect);
+		Application.LoadLevel("Credits");
+	}
+
+	private void HandleBackClick()
+	{
+		audio.PlayOneShot(MainMenuSelect);
+		Application.LoadLevel("Menu");
 	}
 }
